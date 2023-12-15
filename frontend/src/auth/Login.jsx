8e27ab474bddc8797b0d132cd2b15578c8c7dc7b
Agnieszka Kusy <agnieszkakusy25@gmail.com>
2023-12-15 18:57:46 +0100

    Add login and registration form handling functions

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,24 +1,62 @@
+import Cookies from 'js-cookie'
+import { useState } from "react"
+import { performRequest } from '../api/config';
+import { toast } from 'react-toastify';
+
 const Login = () => {
- return(
-      <form className='login-form'>
-          <input
-            type="email"
-            placeholder="Email"
-            id="email"
-            name="email"
-            className="input"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Hasło"
-            id="password"
-            name="password"
-            className="input"
-            required
-          /><br></br>
-        <button type="submit" className="button-card button">LOGOWANIE</button>
-      </form>
- )
+  const [data, setData] = useState({ email: "", password: "" })
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value })
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await performRequest('Accounts/login', 'post', data);
+
+      if (res.token == null) {
+        toast.warn(res.message)
+      }
+      else if (res.token != null && res.token !== undefined) {
+        Cookies.set('token', res.token, {
+          sameSite: 'lax',
+          secure: true
+        })
+        
+        window.location = '/account'
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+    <form className='login-form' onSubmit={handleLogin}>
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        className="input"
+        required
+        onChange={handleChange}
+        value={data.email}
+      />
+      <input
+        type="password"
+        placeholder="Hasło"
+        name="password"
+        className="input"
+        required
+        onChange={handleChange}
+        value={data.password}
+      /><br></br>
+      <button type="submit" className="button-card button">LOGOWANIE</button>
+      
+    </form>
+    </>
+  )
 };
 export default Login;
--- frontend/src/auth/Registration.jsx
+++ frontend/src/auth/Registration.jsx
@@ -1,32 +1,98 @@
+import Cookies from 'js-cookie';
+import { useState } from "react"
+import { performRequest } from '../api/config';
+import { ROLES } from '../const/roles.js';
+import { toast } from 'react-toastify';
+
 const Registration = () => {
-    return(
-         <form className='registration-form'>
-            <input
-               type="text"
-               placeholder="PESEL"
-               id="pesel"
-               name="pesel"
-               className="input"
-               required
-             />
-             <input
-               type="email"
-               placeholder="Email"
-               id="email"
-               name="email"
-               className="input"
-               required
-             />
-             <input
-               type="password"
-               placeholder="Hasło"
-               id="password"
-               name="password"
-               className="input"
-               required
-             /><br></br>
-           <button type="submit" className="button-card button">REJESTRACJA</button>
-         </form>
-    )
-   };
-   export default Registration;
+  const [data, setData] = useState({ pesel: "", email: "", password: "", role: ROLES.PATIENT })
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value })
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const user = await performRequest('Accounts/register', 'post', data);
+
+    if (user.token == null) {
+      toast.warn(user.message)
+    }
+    else if (user.token != null && user.token !== undefined) {
+
+      Cookies.set('token', user.token, {
+        sameSite: 'lax',
+        secure: true
+      })
+
+      window.location = "/personal-form"
+    }
+  }
+
+  return (
+    <form className='registration-form' onSubmit={handleRegister}>
+      <input
+        type="text"
+        placeholder="PESEL"
+        name="pesel"
+        className="input"
+        required
+        onChange={handleChange}
+        value={data.pesel}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        className="input"
+        required
+        onChange={handleChange}
+        value={data.email}
+      />
+      <input
+        type="password"
+        placeholder="Hasło"
+        name="password"
+        className="input"
+        required
+        onChange={handleChange}
+        value={data.password}
+      />
+      <br></br>
+      <label className="radio-auth">
+        <br />
+        <input
+          type="radio"
+          name="role"
+          className="radio-button"
+          value={ROLES.PATIENT}
+          onChange={handleChange}
+          checked={data.role === ROLES.PATIENT}
+        /> Pacjent
+        <span className="checkmark"></span>
+      </label>
+      <input
+        type="radio"
+        name="role"
+        className="radio-button"
+        value={ROLES.DOCTOR}
+        onChange={handleChange}
+        checked={data.role === ROLES.DOCTOR}
+      /> Lekarz
+      <label>
+        <input
+          type="radio"
+          name="role"
+          className="radio-button"
+          value={ROLES.RECEPTIONIST}
+          onChange={handleChange}
+          checked={data.role === ROLES.RECEPTIONIST}
+        /> Rejestracja
+      </label>
+      <br></br>
+      <button type="submit" className="button-card button">REJESTRACJA</button>
+    </form>
+  )
+};
+export default Registration;
