07666d934d32332160a28bef88b7ee1ec8e5ee70
Agnieszka Kusy <agnieszkakusy25@gmail.com>
2023-12-20 23:27:39 +0100

    Add custom statuses

---
+++ backend/Medilo/Medilo.API/Models/CustomStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Medilo.API.Models
+{
+    public static class CustomStatus
+    {
+        public const string Scheduled = "Zaplanowana";
+        public const string Rescheduled = "Przełożona";
+        public const string Canceled= "Anulowana";
+        public const string Completed = "Zakończona";
+    }
+}
